dce2f826a0a487fe347e0952298c3be02d54c6be
Mike OShea <oshea00@gmail.com>
2019-08-24 07:15:57 -0700

    removed uneeded scope claim check

--- StoreService.Web/HasScopeHandler.cs
+++ StoreService.Web/HasScopeHandler.cs
@@ -17,15 +17,8 @@ namespace StoreService.Web
         }
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
         {
-            if (!context.User.HasClaim(c => (c.Type == "scope" || c.Type == "permissions") && c.Issuer == requirement.Issuer))
+            if (!context.User.HasClaim(c => c.Type == "permissions" && c.Issuer == requirement.Issuer))
                 return Task.CompletedTask;
-
-            var scope = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer);
-            if (scope != null)
-            {
-                if (scope.Value.Split(' ').Any(s => s == requirement.Scope))
-                    context.Succeed(requirement);
-            }
 
             if (context.User.HasClaim(c => c.Type == "permissions" && c.Value == requirement.Scope))
                 context.Succeed(requirement);
